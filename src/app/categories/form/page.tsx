871f5b4a4f41e4720f584eb8c212ab54e42a81e1
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-04-02 21:15:03 -0300

    Adicionando cadastro de categorias com validações

--- src/app/categories/form/page.tsx
+++ src/app/categories/form/page.tsx
@@ -2,39 +2,56 @@
 
 import { createCategory } from "@/actions/category-actions";
 import NavBar from "@/components/nav-bar";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useActionState } from "react";
 
 const initialState = {
-    values: {
-        name: "",
-        icon: ""
-    }
-}
-export default function CategoriesFormPage() {
-    const [state, formAction, pending] = useActionState(createCategory, initialState);
-    return (
-        <>
-        <NavBar active="categorias"/>
-            <main className="flex items-center justify-center">
-                <div className=" bg-slate-900 rounded max-w[500px] p-6 m-6">
-                    <h2 className="font-bold">Cadastrar Categoria</h2>
-                    <form action={formAction} className="space-y-4 mt-6">
-                        <Input name="name" placeholder="Nome da categoria"/>
-                        <Input name="icon" placeholder="Ícone"/>
-                        <div className="flex justify-around">
-                            <Button variant="outline" asChild>
-                                <Link href="/categories">
-                                    Cancelar
-                                </Link>
-                            </Button>
-                            <Button>Salvar</Button>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    )
+  value: {
+    name: "",
+    icon: "",
+  },
+  errors: {
+    name: "",
+    icon: ""
+  }
+};
+
+export default function CategoriesForm() {
+  const [state, formAction, pending] = useActionState(
+    createCategory,
+    initialState
+  );
+  return (
+    <>
+      <NavBar active="categorias" />
+      <main className="flex items-center justify-center">
+        <div className="bg-slate-900 rounded p-5 m-6 max-w-[500px]">
+          <h2 className="font-bold">Cadastrar categoria</h2>
+          <form action={formAction} className="space-y-4 mt-6">
+
+            <div>
+                <Input name="name" placeholder="nome da categoria" aria-invalid={!!state?.errors.name} />
+                <span className="text-sm text-destructive">{state?.errors.name}</span>
+            </div>
+
+            <div>
+                <Input name="icon" placeholder="nome do ícone" aria-invalid={!!state?.errors.icon} />
+                <span className="text-sm text-destructive">{state?.errors.icon}</span>
+            </div>
+            
+            
+            <div className="flex justify-around">
+              <Button asChild variant="outline">
+                <Link href="/categories">Cancelar</Link>
+              </Button>
+              <Button>Salvar</Button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
 }
